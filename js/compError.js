d772b38ce4e0580940cc5153fd90fd52a3853a6d
Евген Лавренко <rheingoldgalaxy@gmail.com>
2022-10-20 18:23:03 +0300

    project

--- js/compError.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Vue.component ('error', {
-    template: ` <div>
-                    <h3 class="error-connect">Возникла ошибка при подключении к серверу</h3>
-                </div>`
-})
